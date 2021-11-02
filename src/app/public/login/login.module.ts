96f3b71ec1ff29e978a6acfdbe4309a08b9f6ff2
staner70 <taner2003@gmail.com>
2021-11-02 10:49:42 +0100

    create moduls requit

---
+++ src/app/public/login/login.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule
+  ]
+})
+export class LoginModule { }

6fbc2595eca683ef3730be0a3c696d4f8d611b2c
staner70 <taner2003@gmail.com>
2021-11-23 10:56:30 +0100

    add service reload

---
+++ src/app/core/components/loader/loader.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoaderService } from '../../service/loader.service';
+
+@Component({
+  selector: 'al-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent implements OnInit {
+
+  isLoading$: Observable<boolean>;
+
+  constructor(private loaderService: LoaderService) { }
+
+  ngOnInit(): void {
+    this.isLoading$ = this.loaderService.isLoading$;
+  }
+
+}

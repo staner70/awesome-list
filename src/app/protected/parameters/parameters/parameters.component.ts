89cfe0560ae99571cca0f40f5d42d18a45dbc678
staner70 <taner2003@gmail.com>
2021-12-02 15:18:11 +0100

    add parameter form

--- src/app/protected/parameters/parameters/parameters.component.ts
+++ src/app/protected/parameters/parameters/parameters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'al-parameters',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ParametersComponent implements OnInit {
+  parametersForm: FormGroup;
+  pomodoros: number[] = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
 
-  constructor() { }
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.parametersForm = this.fb.group({
+      pomodoro: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.parametersForm.get('pomodoro')?.value);
   }
 
 }
